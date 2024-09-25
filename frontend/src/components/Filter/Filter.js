fb61034795e931cb7f3b1b9081b8dede146da044
woocle1 <andriytiuska@gmail.com>
2024-09-25 23:28:35 +0300

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Filter.css'
+
+const Filter = () => {
+  return (
+    <div className="app-block filer">
+      <h2>Filters</h2>
+    </div>
+  )
+}
+
+export default Filter
